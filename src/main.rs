b188eaa7fdc67dbdf04e768c79b76d310afdc247
Anastasy Belyaev <Anastasy.Belyaev@kaspersky.com>
2025-02-22 18:43:43 +0300

    WebSocket echo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use futures_util::{SinkExt, StreamExt};
+use tokio_tungstenite::{connect_async, tungstenite::Message};
+
+#[tokio::main]
+async fn main() {
+    let url = "wss://ws.postman-echo.com/raw/";
+    println!("Connecting to {}", url);
+    let (ws, _) = connect_async(url).await.expect("Failed to connect");
+    println!("Connected");
+
+    let (mut ws_write, mut ws_read) = ws.split();
+
+    ws_write.send(Message::Text("Hello WebSocket".into())).await.expect("Failed to send message");
+    ws_write.send(Message::Text("Second message".into())).await.expect("Failed to send message");
+    while let Some(msg) = ws_read.next().await {
+        let msg = msg.unwrap();
+        match msg {
+            Message::Text(txt) => println!("Received: {}", txt),
+            Message::Close(_) => {
+                println!("Connection closed");
+                break;
+            }
+            _ => (),
+        }
+    }
 }
